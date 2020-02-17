8f992a478fdc83f993b5080fd2472e26561816b6
Begin <support@begin.com>
2020-02-17 16:07:35 +0100

    _ci: adds src/http/get-api-posts/index.js

--- src/http/get-api-posts/index.js
+++ src/http/get-api-posts/index.js
@@ -1,15 +1,39 @@
-// learn more about HTTP functions here: https://arc.codes/primitives/http
-// system imports
-// 3rd party imports
-const data = require('@begin/data')
-// local imports
+// Enable secure sessions, express-style middleware, and more:
+// https://docs.begin.com/en/functions/http/
+//
+// let begin = require('@architect/functions')
 
+let html = `
+<!doctype html>
+<html lang=en>
+  <head>
+    <meta charset=utf-8>
+    <title>Hi!</title>
+    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
+    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon" />
+  </head>
+  <body>
+    <h1 class="center-text">
+      Hello world!
+    </h1>
+    <p class="center-text">
+      Your new route is ready to go!
+    </p>
+    <p class="center-text">
+      Learn more about building <a href="https://docs.begin.com/en/functions/http/" class="link" target="_blank">Begin HTTP functions here</a>.
+    </p>
+  </body>
+</html>
+`
+
+// HTTP function
 exports.handler = async function http(req) {
-  const table = 'posts'
-  const posts = await data.get({ table })
-
+  console.log(req)
   return {
-    statusCode: 200,
-    body: JSON.stringify(posts),
+    headers: {
+      'content-type': 'text/html; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
+    body: html
   }
 }
